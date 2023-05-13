9ac8388d9f39aebaf727415b4c862ca2c28b6930
Anastasia Koroleva <a.koroleva@loftice.ru>
2023-05-13 15:01:47 +0300

    fix: правки по фронтэнду, убрана проверка с типизацией данных в условиях

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,7 +4,6 @@ import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
 function Main(props) {
   const currentUser = useContext(CurrentUserContext);
-  console.log(props.cards);
 
   return (
     <main className="content">
